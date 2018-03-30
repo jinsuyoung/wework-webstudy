e8501a83d3d074f98f7ab9b22a48e48c725ed774
jinsuyoung <jinsy7722@naver.com>
2018-03-30 17:01:48 +0900

    첫번째 커밋

---
+++ test3.php
@@ -0,0 +1,14 @@
+<?php
+$var1 = "wework";
+echo $var1;
+
+$var2 = "서울아이티고등학교";
+echo $var2;
+
+echo $var1.$var2;
+
+echo "<br>";
+
+echo $var1. "<br>". $var2;
+
+?>
---
+++ test6.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<head>
+    <meta charset = "utf-8">
+</head>
+<body>
+<form action="action2.php" method="post">
+    <input type="text" name="user_name" placeholder="아이디 입력"
+    required>
+    <input type="password" name="user_password" placeholder="비밀번호 입력"
+    required>
+    <button type="submit">전송</button>
+</form>
+</body>
+</html>
+
